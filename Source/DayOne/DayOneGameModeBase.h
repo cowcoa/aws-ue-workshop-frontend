7eb02a61afdb9515cf85d455ec362996fc37eb92
Xin Zhang <zxaws@amazon.com>
2022-11-25 17:51:51 +0800

    first commit.
    unreal engine 5.1.0 empty project.

---
+++ Source/DayOne/DayOneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DayOneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DAYONE_API ADayOneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
