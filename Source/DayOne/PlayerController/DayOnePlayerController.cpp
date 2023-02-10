2f34a681375b8b60763cb47a9ed81239ebe8a9c5
Xin Zhang <zxaws@amazon.com>
2023-02-10 18:48:57 +0800

    make server time sync with all client and display it on BattleField overlay UI.

--- Source/DayOne/PlayerController/DayOnePlayerController.cpp
+++ Source/DayOne/PlayerController/DayOnePlayerController.cpp
@@ -80,6 +80,34 @@ void ADayOnePlayerController::SetHUDCarriedAmmo(int32 Ammo)
 	}
 }
 
+float ADayOnePlayerController::GetServerTime()
+{
+	if (HasAuthority()) return GetWorld()->GetTimeSeconds();
+	else return GetWorld()->GetTimeSeconds() + ClientServerDelta;
+}
+
+void ADayOnePlayerController::SetHUDMatchCountdown(float CountdownTime)
+{
+	CombatHUD = CombatHUD == nullptr ? Cast<ACombatHUD>(GetHUD()) : CombatHUD;
+	bool bHUDValid = CombatHUD &&
+		CombatHUD->CharacterOverlay &&
+		CombatHUD->CharacterOverlay->MatchCountdownText;
+	if (bHUDValid)
+	{
+		if (CountdownTime < 0.f)
+		{
+			CombatHUD->CharacterOverlay->MatchCountdownText->SetText(FText());
+			return;
+		}
+
+		int32 Minutes = FMath::FloorToInt(CountdownTime / 60.f);
+		int32 Seconds = CountdownTime - Minutes * 60;
+
+		FString CountdownText = FString::Printf(TEXT("%02d:%02d"), Minutes, Seconds);
+		CombatHUD->CharacterOverlay->MatchCountdownText->SetText(FText::FromString(CountdownText));
+	}
+}
+
 void ADayOnePlayerController::BeginPlay()
 {
 	Super::BeginPlay();
@@ -91,6 +119,8 @@ void ADayOnePlayerController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	SetHUDTime();
+	CheckTimeSync(DeltaTime);
 	PollInit();
 }
 
@@ -103,6 +133,29 @@ void ADayOnePlayerController::OnPossess(APawn* InPawn)
 	{
 		SetHUDHealth(DayOneCharacter->GetHealth(), DayOneCharacter->GetMaxHealth());
 	}
+}
+
+void ADayOnePlayerController::ReceivedPlayer()
+{
+	Super::ReceivedPlayer();
+
+	if (IsLocalController())
+	{
+		ServerRequestServerTime(GetWorld()->GetTimeSeconds());
+	}
+}
+
+void ADayOnePlayerController::SetHUDTime()
+{
+	float TimeLeft = MatchTime - GetServerTime();
+	uint32 SecondsLeft = FMath::CeilToInt(TimeLeft);
+
+	if (CountdownInt != SecondsLeft)
+	{
+		SetHUDMatchCountdown(TimeLeft);
+	}
+
+	CountdownInt = SecondsLeft;
 }
 
 void ADayOnePlayerController::PollInit()
@@ -120,4 +173,29 @@ void ADayOnePlayerController::PollInit()
 			}
 		}
 	}
+}
+
+void ADayOnePlayerController::CheckTimeSync(float DeltaTime)
+{
+	TimeSyncRunningTime += DeltaTime;
+	if (IsLocalController() && TimeSyncRunningTime > TimeSyncFrequency)
+	{
+		ServerRequestServerTime(GetWorld()->GetTimeSeconds());
+		TimeSyncRunningTime = 0.f;
+	}
+}
+
+void ADayOnePlayerController::ServerRequestServerTime_Implementation(float TimeOfClientRequest)
+{
+	float ServerTimeOfReceipt = GetWorld()->GetTimeSeconds();
+	ClientReportServerTime(TimeOfClientRequest, ServerTimeOfReceipt);
+}
+
+void ADayOnePlayerController::ClientReportServerTime_Implementation(float TimeOfClientRequest,
+	float TimeServerReceivedClientRequest)
+{
+	float RoundTripTime = GetWorld()->GetTimeSeconds() - TimeOfClientRequest;
+	SingleTripTime = 0.5f * RoundTripTime;
+	float CurrentServerTime = TimeServerReceivedClientRequest + SingleTripTime;
+	ClientServerDelta = CurrentServerTime - GetWorld()->GetTimeSeconds();
 }
--- Source/DayOne/PlayerController/DayOnePlayerController.h
+++ Source/DayOne/PlayerController/DayOnePlayerController.h
@@ -19,13 +19,40 @@ public:
 	void SetHUDScore(float Score);
 	void SetHUDWeaponAmmo(int32 Ammo);
 	void SetHUDCarriedAmmo(int32 Ammo);
+	// countdown game over time
+	virtual float GetServerTime(); // Synced with server world clock
+	void SetHUDMatchCountdown(float CountdownTime);
 
 protected:
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
 	virtual void OnPossess(APawn* InPawn) override;
+	virtual void ReceivedPlayer() override; // Sync with server clock as soon as possible
 
+	void SetHUDTime();
 	void PollInit();
+
+	/**
+* Sync time between client and server
+*/
+
+	// Requests the current server time, passing in the client's time when the request was sent
+	UFUNCTION(Server, Reliable)
+	void ServerRequestServerTime(float TimeOfClientRequest);
+
+	// Reports the current server time to the client in response to ServerRequestServerTime
+	UFUNCTION(Client, Reliable)
+	void ClientReportServerTime(float TimeOfClientRequest, float TimeServerReceivedClientRequest);
+
+	float ClientServerDelta = 0.f; // difference between client and server time
+
+	UPROPERTY(EditAnywhere, Category = Time)
+	float TimeSyncFrequency = 5.f;
+
+	float TimeSyncRunningTime = 0.f;
+	void CheckTimeSync(float DeltaTime);
+
+	float SingleTripTime = 0.f;
 
 private:
 	UPROPERTY()
@@ -40,4 +67,8 @@ private:
 	bool bInitializeWeaponAmmo = false;
 	float HUDCarriedAmmo;
 	bool bInitializeCarriedAmmo = false;
+
+	// time of the game
+	float MatchTime = 120.f;
+	uint32 CountdownInt = 0;
 };
